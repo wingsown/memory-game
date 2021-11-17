a306acd5b50c1a5d70dbad597b62a349c414c316
Wilson <wj.buena@gmail.com>
2021-11-17 09:35:58 +0800

    Stored initial state of cards

---
+++ src/features/counter/board/boardSlice.js
@@ -0,0 +1,14 @@
+const initialState = [
+    {id: 0, contents: 'Provider', visibile: true, matched: true},
+    {id: 1, contents: 'Provider', visibile: true, matched: true},
+    {id: 2, contents: 'selector', visibile: true, matched: true},
+    {id: 3, contents: 'selector', visibile: true, matched: true},
+    {id: 4, contents: 'useSelector()', visibile: true, matched: true},
+    {id: 5, contents: 'useSelector()', visibile: true, matched: true},
+    {id: 6, contents: 'useDispatch()', visibile: true, matched: true},
+    {id: 7, contents: 'useDispatch()', visibile: true, matched: true},
+    {id: 8, contents: 'Pure Function', visibile: true, matched: true},
+    {id: 9, contents: 'Pure Function', visibile: true, matched: true},
+    {id: 10, contents: 'react-redux', visibile: true, matched: true},
+    {id: 11, contents: 'react-redux', visibile: true, matched: true},
+]
