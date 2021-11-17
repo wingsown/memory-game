e2e3b018b8bb575306eba9fa33f90389a7f4fc41
Wilson <wj.buena@gmail.com>
2021-11-17 09:15:39 +0800

    Root component JSX

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ import { useDispatch } from 'react-redux';
 
 const App = () => {
   const dispatch = useDispatch();
+
+
+  return (
+    <div className="App">
+      <footer>
+        <button>
+          Start Game
+        </button>
+        <button>
+          Try New Pair
+        </button>
+      </footer>
+    </div>
+  )
 }
 
 export default App;
