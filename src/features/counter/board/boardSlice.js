85236eb8040ee3c4f1df06602ef21caa3c96c5cd
Wilson <wj.buena@gmail.com>
2021-11-17 10:37:24 +0800

    export functions

--- src/features/counter/board/boardSlice.js
+++ src/features/counter/board/boardSlice.js
@@ -1,16 +1,16 @@
 const initialState = [
-    {id: 0, contents: 'Provider', visibile: true, matched: true},
-    {id: 1, contents: 'Provider', visibile: true, matched: true},
-    {id: 2, contents: 'selector', visibile: true, matched: true},
-    {id: 3, contents: 'selector', visibile: true, matched: true},
-    {id: 4, contents: 'useSelector()', visibile: true, matched: true},
-    {id: 5, contents: 'useSelector()', visibile: true, matched: true},
-    {id: 6, contents: 'useDispatch()', visibile: true, matched: true},
+    {id: 0, contents: 'Provider', visible: true, matched: true},
+    {id: 1, contents: 'Provider', visible: true, matched: true},
+    {id: 2, contents: 'selector', visible: true, matched: true},
+    {id: 3, contents: 'selector', visible: true, matched: true},
+    {id: 4, contents: 'useSelector()', visible: true, matched: true},
+    {id: 5, contents: 'useSelector()', visible: true, matched: true},
+    {id: 6, contents: 'useDispatch()', visible: true, matched: true},
     {id: 7, contents: 'useDispatch()', visibile: true, matched: true},
-    {id: 8, contents: 'Pure Function', visibile: true, matched: true},
-    {id: 9, contents: 'Pure Function', visibile: true, matched: true},
-    {id: 10, contents: 'react-redux', visibile: true, matched: true},
-    {id: 11, contents: 'react-redux', visibile: true, matched: true},
+    {id: 8, contents: 'Pure Function', visible: true, matched: true},
+    {id: 9, contents: 'Pure Function', visible: true, matched: true},
+    {id: 10, contents: 'react-redux', visible: true, matched: true},
+    {id: 11, contents: 'react-redux', visible: true, matched: true},
 ];
 
 export const boardReducer = (state = initialState, action) => {
@@ -102,3 +102,11 @@ export const resetCards = (indices) => {
         type: 'board/resetCards'
     }
 }
+
+export const selectBoard = state => state.board.map(card => ({
+    id: card.id,
+    contents: card.contents
+}));
+
+export const selectVisibleIds = state => state.board.filter(card => card.visible).map(card => card.id);
+export const selectMatchedIds = state => state.board.filter(card => card.matched).map(card => card.id);
