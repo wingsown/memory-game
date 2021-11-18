04047e44e74c7aeca125236491a7d3234c49f57a
Wilson <wj.buena@gmail.com>
2021-11-18 10:30:53 +0800

    Card component variables

--- src/features/counter/board/cardRow/card/Card.js
+++ src/features/counter/board/cardRow/card/Card.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { selectVisibleIds, flipCard, selectMatchedIds } from '../../boardSlice';
+
+let cardLogo = "https://static-assets.codecademy.com/Courses/Learn-Redux/matching-game/codecademy_logo.png";
+
+export const Card = ({ id, contents }) => {
+    
+    const dispatch = useDispatch();
+    const visibleIds = useSelector(selectVisibleIds);
+    const matchedIds = useSelector(selectMatchedIds);
+}
