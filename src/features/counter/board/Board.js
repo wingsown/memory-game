07844626178c97e7fd001958694487e9952c22da
Wilson <wj.buena@gmail.com>
2021-11-18 09:42:39 +0800

    variables for board component

--- src/features/counter/board/Board.js
+++ src/features/counter/board/Board.js
@@ -2,4 +2,13 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectBoard } from './boardSlice';
 
-export const Board = () => {}
+export const Board = () => {
+
+    const currentBoard = useSelector(selectBoard);
+
+    const numberOfCards = currentBoard.length;
+    const columns = 3;
+    const rows = Math.floor(numberOfCards / columns);
+
+    
+}
