084dc4d4f6dc21e870816758bd25bd84c803eea2
Wilson <wj.buena@gmail.com>
2021-11-19 09:10:13 +0800

    import statements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
+import { Score } from './features/counter/board/score/Score';
+import { Board } from './features/counter/board/Board';
 import { useDispatch } from 'react-redux';
+import { setBoard, resetCards } from './features/counter/board/boardSlice';
+
 
 const App = () => {
   const dispatch = useDispatch();
