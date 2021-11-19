9f355cbb2819d35454790df390737f4c63ee00f9
Wilson <wj.buena@gmail.com>
2021-11-19 10:25:14 +0800

    rendering complete

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,4 @@
 import { combineReducers, createStore } from 'redux';
-import { configureStore } from '@reduxjs/toolkit';
 import { boardReducer } from '../features/board/boardSlice';
 
 const rootReducer = combineReducers({
