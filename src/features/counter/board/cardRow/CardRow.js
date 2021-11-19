b33fdd92813e2280261f8e0ab100c029c5e3b727
Wilson <wj.buena@gmail.com>
2021-11-19 09:01:16 +0800

    Cardrow Component complete

--- src/features/counter/board/cardRow/CardRow.js
+++ src/features/counter/board/cardRow/CardRow.js
@@ -1,4 +1,13 @@
 import React from 'react';
 import { Card } from './card/Card';
 
-export const CardRow = ({ cards }) => {}
+export const CardRow = ({ cards }) => {
+    const content = cards.map(card => 
+        <Card
+        key={card.id}
+        id={card.id}
+        contents={card.contents}
+        />)
+
+        return <>{content}</>;
+};
